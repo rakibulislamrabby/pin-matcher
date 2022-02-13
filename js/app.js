0b42a5fdd476e8203e6254f3ec0deee08317fab6
Rakibulislamrabby <rakibulislamrabby64@gmai.com>
2022-02-13 21:46:25 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + "";
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+function generatePin() {
+    const pin = getPin();
+    document.getElementById("display-pin").value = pin;
+}
+
+document.getElementById("key-pad").addEventListener("click", function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById("typed-numbers");
+    if (isNaN(number)) {
+        if (number == "C") {
+            calcInput.value = "";
+        }
+    }
+    else {
+
+        const previouseNumber = calcInput.value;
+        const newNumber = previouseNumber + number;
+        calcInput.value = newNumber;
+    }
+
+});
+
+function verifyPin() {
+    const pin = document.getElementById("display-pin").value;
+    const typedNumber = document.getElementById("typed-numbers").value;
+    const successnotify = document.getElementById("notify-success");
+    const failnotify = document.getElementById("notify-fail");
+    if (pin == typedNumber) {
+
+        successnotify.style.display = "block"
+        failnotify.style.display = "none";
+    }
+    else {
+
+        failnotify.style.display = "block";
+        successnotify.style.display = "none"
+    }
+}
